0c6b84513fd793397cde1341b85f835f6c9ace8c
Karson Bryant <101122824+KarBryant@users.noreply.github.com>
2024-08-01 07:15:56 -0500

    Added my code to this project.
    I have completed my code before creating this repository. Uploading my code now. In future projects I should incorporate version control.

---
+++ Main.py
@@ -0,0 +1,80 @@
+
+def add(num1,num2):
+    result = num1 + num2 
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+def divide(num1,num2):
+    result = num1 / num2
+    return result
+
+def remainder(num1,num2):
+    result = num1 % num2
+    return result
+
+def getNum(prompt="Please enter your number. \n"):
+    while True:
+        user_input = input(prompt)
+        try:
+            user_num = float(user_input)
+            return user_num
+        except ValueError:
+            print("This is not a number. Pleaes try again.")
+
+
+def getOperator(prompt="enter your operator (+, -, *, /) \n"):
+
+    operators = ["+", "-", "*", "/"]
+
+    while True:
+        user_input = input(prompt)
+        if user_input in operators:
+            return user_input
+        else:
+            print("this is an inv`alid operator. please enter one of the following: \n (+, -, *, /)")
+
+
+def calculation(num1,num2, operation):
+    if operation == "+":
+        print("Result:", add(num1,num2), "\n")
+    
+    elif operation == "-":
+        print("Result:", subtract(num1,num2), "\n")
+    
+    elif operation == "*":
+        print("Result:", multiply(num1,num2), "\n")
+
+    elif operation == "/":
+        print("Result:", divide(num1,num2), "\n")
+    
+    else:
+        print("this is invalid. try again.")
+
+if __name__ == "__main__":
+
+    calculate = True
+    print("--------------------------------------------- \n")
+    print("how this works: \n enter 2 numbers and the operation. \n")
+    print("---------------------------------------------")
+
+    while calculate == True:
+        
+        num1 = getNum()
+        num2 = getNum()
+        op = getOperator()
+
+        calculation(num1,num2, op)
+
+        contin = input("continue Y/N \n")
+        if ((contin == "Y") or (contin== "y")):
+            continue
+        elif ((contin == "N") or (contin=="n")):
+            calculate = False
+        
